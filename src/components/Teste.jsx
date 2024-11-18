dff7406103a9e51f1818c50d72fde7fbde13a1de
Matheus Ursino <matheusursino008@gmail.com>
2024-11-18 17:05:23 -0300

    juju narutooooooo

--- src/components/Teste.jsx
+++ src/components/Teste.jsx
@@ -3,6 +3,8 @@ export default function Teste() {
         <>
             <h1>Teste</h1>
             <p>teste teste teste e mais um teste teste aaaaaaaa</p>
+            <p>eu vou dar meu e meu bucetao</p>
+            <p>juju naruto porraaaaaaaaaaa</p>
         </>
     )
 }
